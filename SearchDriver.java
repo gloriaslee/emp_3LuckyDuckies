1ddef8672c26215f1450ef9e1d353f3f5ef457cb
gloriaslee <glee20@stuy.edu>
2021-12-16 22:26:07 -0500

    update

--- SearchDriver.java
+++ SearchDriver.java
@@ -3,25 +3,31 @@ import java.util.ArrayList;
 public class SearchDriver{
 
   public static boolean insert(ArrayList<Integer> arr, Integer value) { // this is a function that adds each element to teh array
-     if (r.size() == 0){
-       this._arr.add(value);
-     } else if (this._arr.get(0) > value) {
-			this._arr.add(0,value);
+     if (arr.size() == 0){
+       arr.add(value);
+     } else if (arr.get(0) > value) {
+			arr.add(0,value);
 			return true;
 
-    }	else if (value > this._arr.get(this._arr.size()-1)) {
-			this._arr.add(value);
+    }	else if (value > arr.get(arr.size()-1)) {
+			arr.add(value);
 			return true;
 
     }	else {
-			for(int i = 0; i < this._arr.size(); i++) {
-				if (this._arr.get(i) <= value && this._arr.get(i+1) >= value) { //should this be >= and <= or just > and <
-					this._arr.add(i+1, value);
+			for(int i = 0; i < arr.size(); i++) {
+				if (arr.get(i) < value && arr.get(i+1) > value) { //should this be >= and <= or just > and <
+					arr.add(i+1, value);
 					return true;}
 				}
 			}
 			return false;
 		}//end addLinear
+
+
+
+
+
+
 
   public static void main(String[] args) {
 
@@ -37,11 +43,18 @@ public class SearchDriver{
  }
    */
 
+   //~~==============TESTING STRINGS
+   /*
+
+   */
+
+
+
     //~~==============TESTING INTEGER
     startTime = System.currentTimeMillis();
     ArrayList<Integer> IntArr1 = new ArrayList<Integer>(1000000); // 1 million
     for (int i = 0; i < 1000000; i++){
-      b.add( (int) (Math.random() * 101) );
+      IntArr1.add( (int) (Math.random() * 101) );
     }
     long endTime = System.currentTimeMillis();
     System.out.println(endTime - startTime); // elapsed time in milliseconds
@@ -50,17 +63,23 @@ public class SearchDriver{
     startTime = System.currentTimeMillis();
     ArrayList<Integer> IntArr2 = new ArrayList<Integer>(1000000);
     for (int i = 0; i < 1000000; i++){
-      c.add( (int) (Math.random() * 701) );
+      IntArr2.add( (int) (Math.random() * 701) );
     }
     endTime = System.currentTimeMillis();
     System.out.println(endTime - startTime);
 
 
+    startTime = System.currentTimeMillis();
+    // sets up an ordered test case
     ArrayList<Integer> IntArr3 = new ArrayList<Integer>(1000000);
     for (int i = 0; i < 1000000; i++){
-      Integer val = (int) Math.random() * 1000000;
+      Integer val = (int) (Math.random() * 1000000);
       insert(IntArr3, val);
     }
+    System.out.println(IntArr3);
+  // N: With length 100 AND the print statement, it takes about 1-2 ms
+  // N: With length 100 AND WITHOUT the print statement, it takes about 0-1 ms
+    endTime = System.currentTimeMillis();
+    System.out.println(endTime - startTime);
   }
-  //public static long currentTimeMillis();
 }
