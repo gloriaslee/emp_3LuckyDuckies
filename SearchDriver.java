48759162d89ee6e97383585df09d37e9f7b5095c
gloriaslee <glee20@stuy.edu>
2021-12-16 22:37:48 -0500

    push

--- SearchDriver.java
+++ SearchDriver.java
@@ -2,31 +2,68 @@ import java.util.ArrayList;
 
 public class SearchDriver{
 
-  public static boolean insert(ArrayList<Integer> arr, Integer value) { // this is a function that adds each element to teh array
-     if (arr.size() == 0){
-       arr.add(value);
-     } else if (arr.get(0) > value) {
-			arr.add(0,value);
-			return true;
+  // public static boolean insert(ArrayList<Integer> arr, Integer value) { // this is a function that adds each element to teh array
+  //    if (arr.size() == 0){
+  //      arr.add(value);
+  //    } else if (arr.get(0) > value) {
+	// 		arr.add(0,value);
+	// 		return true;
+  //
+  //   }	else if (value > arr.get(arr.size()-1)) {
+	// 		arr.add(value);
+	// 		return true;
+  //
+  //   }	else {
+	// 		for(int i = 0; i < arr.size(); i++) {
+	// 			if (arr.get(i) < value && arr.get(i+1) > value) { //should this be >= and <= or just > and <
+	// 				arr.add(i+1, value);
+	// 				return true;}
+	// 			}
+	// 		}
+	// 		return false;
+	// 	}//end addLinear
 
-    }	else if (value > arr.get(arr.size()-1)) {
-			arr.add(value);
-			return true;
+    public static void insert2(ArrayList<Integer> arr, Integer newVal)
+      {
+        //initialize high, low, midpt indices
+        int lo = 0;
+        int med = 0;
+        int hi = arr.size()-1;
 
-    }	else {
-			for(int i = 0; i < arr.size(); i++) {
-				if (arr.get(i) < value && arr.get(i+1) > value) { //should this be >= and <= or just > and <
-					arr.add(i+1, value);
-					return true;}
-				}
-			}
-			return false;
-		}//end addLinear
+        while ( lo <= hi ) { //running until target is found or bounds cross
 
+          med = (lo + hi) / 2;
+          int x = arr.get(med).compareTo( newVal );
 
+          if ( x == 0 ) {
+            //equal value found, insert here
+            arr.add( med, newVal );
+            return;
+          }
+          else if ( x > 0 )
+            //newVal < med, so look at lower half of data
+            hi = med - 1;
+          else
+            //newVal > med, so look at upper half of data
+            lo = med + 1;
+        }
+        // If you made it this far, newVal is not present.
+        // So insert at lo.
+        arr.add( lo, newVal );
+        //Q: How do you know lo is correct insertion index?
+      }
 
-
-
+/*
+ArrayList<Integer> makeTestCase - wrapper method to streamline test case creation
+*/
+  public static ArrayList<Integer> makeTestCase(int size) {
+    ArrayList<Integer> arr = new ArrayList<Integer>(size);
+    for (int i = 0; i < size; i++){
+      Integer val = (int) (Math.random() * size);
+      insert2(arr, val);
+    }
+     return arr;
+  }
 
 
   public static void main(String[] args) {
@@ -42,13 +79,6 @@ public class SearchDriver{
     a[i] = i;
  }
    */
-
-   //~~==============TESTING STRINGS
-   /*
-
-   */
-
-
 
     //~~==============TESTING INTEGER
     startTime = System.currentTimeMillis();
@@ -71,15 +101,16 @@ public class SearchDriver{
 
     startTime = System.currentTimeMillis();
     // sets up an ordered test case
-    ArrayList<Integer> IntArr3 = new ArrayList<Integer>(1000000);
-    for (int i = 0; i < 1000000; i++){
-      Integer val = (int) (Math.random() * 1000000);
-      insert(IntArr3, val);
-    }
+    ArrayList<Integer> IntArr3 = makeTestCase(100000);
     System.out.println(IntArr3);
   // N: With length 100 AND the print statement, it takes about 1-2 ms
   // N: With length 100 AND WITHOUT the print statement, it takes about 0-1 ms
+  // N: the time it takes seems to be growing exponentially
     endTime = System.currentTimeMillis();
     System.out.println(endTime - startTime);
+
+
   }
+
+
 }
